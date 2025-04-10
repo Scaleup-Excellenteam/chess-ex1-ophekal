cd9734aec235e82e6e2bfa7e2fc6c3fab1143c86
Ophek Alon <ophekal@std.hac.ac.il>
2025-04-10 17:06:33 +0300

    continue building the validator class

--- Chess/src/MovementValidator.cpp
+++ Chess/src/MovementValidator.cpp
@@ -19,21 +19,71 @@ bool MovementValidator::isPathClear(const Piece* piece, const std::string& targe
     std::string pieceName = piece->getName();
     
     // King moves one square so no path to check
-    if (pieceName = "King") {
+    if (pieceName == "King") {
         return true;
     }
 
     auto [startRow, startCol] = piece->positionToCoords(piece->getPosition());
     auto [endRow, endCol] = piece->positionToCoords(targetPosition);
 
+    // virtcal or horizontal path
+    if (startRow == endRow || startCol == endCol) {
+        return isStraightPathClear(startRow, startCol, endRow, endCol, board);
+    }
+
+    // diagonal path
+    if (std::abs(endRow - startRow) == std::abs(endCol - startCol)) {
+        return isDiagonalPathClear(startRow, startCol, endRow, endCol, board);
+    }
+
+    // will not reach here
+    return false;
+}
+
+
+bool MovementValidator::isStraightPathClear(int startRow, int startCol, int endRow, int endCol, const BoardMap& board) {
+
+    if (startRow == endRow) {
+        // since the move is horizontal only the col changes
+        int start = std::min(startCol, endCol) + 1;
+        int end = std::max(startCol, endCol);
+
+        for (int col = start; col < end; col++) {
+            std::string positionToCheck = coordsToPosition(startRow, col);
+
+            if (board.find(positionToCheck) != board.end()) {
+                // means there's a piece in the way
+                return false;
+            }
+        }
+    }
+    else {
+        // the movement is vertical
+        int start = std::min(startRow, endRow) + 1;
+        int end = std::max(startRow, endRow);
+
+        for (int row = start; row < end; row++) {
+            std::string positionToCheck = coordsToPosition(row, startCol);
+
+            if (board.find(positionToCheck) != board.end()) {
+                // means there's a piece in the way
+                return false;
+            }
+        }
+    }
+
+    return true; // path is clear
+}
+
+
+bool MovementValidator::isDiagonalPathClear(int startRow, int startCol, int endRow, int endCol, const BoardMap& board) {
+    return false;
 
 }
 
 
-bool MovementValidator::isStraightPathClear(int startRow, int startCol, int endRow, int endCol, const BoardMap& board) {}
-
-
-bool MovementValidator::isDiagonalPathClear(int startRow, int startCol, int endRow, int endCol, const BoardMap& board) {}
-
-
-std::string MovementValidator::coordsToPosition(int row, int col) {}
+std::string MovementValidator::coordsToPosition(int row, int col) {
+    char rowChar = 'a' + row;
+    char colChar = '1' + col;
+    return std::string(1, rowChar) + std::string(1, colChar);
+}
